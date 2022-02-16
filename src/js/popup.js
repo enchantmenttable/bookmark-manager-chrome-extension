26036246002c90a6e8e094cfd060c6d6dd703f1b
enchantmenttable <namson244@gmail.com>
2022-02-16 15:11:13 +0700

    yeah

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,21 +1,17 @@
 async function readCurrentSiteInfo() {
     return new Promise((resolve, reject) => {
         chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
-            resolve([tabs[0].url, tabs[0].title]);
+            resolve([tabs[0].url, tabs[0].title, `https://www.google.com/s2/favicons?domain=${tabs[0].url}&sz=64`]);
         })
     })
 }
 
 function savePageInfo(url, title) {
-    chrome.storage.local.set({ [url]: title })
+    chrome.storage.local.set({ [url]: title }, (data) => {
+        alert("yo");
+    })
 }
 
-let tabUrl, tabTitle;
-async function init() {
-    let tabInfo = await readCurrentSiteInfo();
-    tabUrl = tabInfo[0];
-    tabTitle = tabInfo[1];
-}
 
 const saveButton = document.getElementById("save");
 saveButton.addEventListener("click", () => {
@@ -27,4 +23,23 @@ openEditorButton.addEventListener("click", () => {
     chrome.tabs.create({ url: "src/html/editor.html" });
 })
 
+function displaySiteInfo(tabUrl, tabTitle, tabFavIconURL, siteInfoContainer) {
+    const siteFavIcon = siteInfoContainer.querySelector("#site-fav-icon");
+    const siteTitle = siteInfoContainer.querySelector("#site-title");
+    const siteUrl = siteInfoContainer.querySelector("#site-url");
+
+    siteFavIcon.setAttribute("src", tabFavIconURL);
+    siteTitle.textContent = tabTitle;
+    siteUrl.setAttribute("value", tabUrl);
+
+}
+
+async function init() {
+    let tabInfo = await readCurrentSiteInfo();
+    const siteInfoContainer = document.getElementById("site-info-container");
+    displaySiteInfo(tabInfo[0], tabInfo[1], tabInfo[2], siteInfoContainer);
+}
+
 init();
+
+
